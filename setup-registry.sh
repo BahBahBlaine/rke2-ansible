cd60b2b52188fce7cddf52cbb72addad394eb7a9
blaine chatman <Chatman_Blaine@bah.com>
2022-02-15 11:48:54 -0700

    Fix ironbank example and update registry setup script

--- setup-registry.sh
+++ setup-registry.sh
@@ -1,31 +1,48 @@
 #!/usr/bin/env bash
 
+set -e
+set -o nounset
+set -o pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# cd to the same dir this script is in for relative paths to work properly
+cd $SCRIPT_DIR
+
+
 # change this to the ip address for the node or dns name for a multi-node setup
 address=localhost:5000
 
 # can also use podman or similar instead
-docker run -d -p 5000:5000 --name registry registry:2
+docker run -d -p 5000:5000 --name registry registry:2 || /bin/true
 
-ironbank_images=(
-    klipper-helm:v0.6.1-build20210616
-    calico:v3.13.3-build20210223
-    coredns:v1.6.9-build20210223
-    flannel:v0.13.0-rancher1-build20210223
-    k8s-metrics-server:v0.3.6-build20210223
+registry_images=(
+    registry1.dso.mil/ironbank/rancher-federal/rke2/klipper-helm:v0.6.1-build20210616
+    registry1.dso.mil/ironbank/rancher-federal/rke2/calico:v3.13.3-build20210223
+    registry1.dso.mil/ironbank/rancher-federal/rke2/coredns:v1.6.9-build20210223
+    registry1.dso.mil/ironbank/rancher-federal/rke2/flannel:v0.13.0-rancher1-build20210223
+    registry1.dso.mil/ironbank/rancher-federal/rke2/k8s-metrics-server:v0.3.6-build20210223
+    registry1.dso.mil/ironbank/rancher-federal/rke2/etcd:v3.4.13-k3s1-build20210223
+    registry1.dso.mil/ironbank/rancher-federal/rke2/hardened-kubernetes:v1.21.3-rke2r1-build20210721
+    registry1.dso.mil/ironbank/rancher-federal/rke2/pause:3.5
+    registry1.dso.mil/ironbank/rancher-federal/rke2/rke2-runtime:v1.21.3-rke2r1
+    docker.io/rancher/rke2-cloud-provider:v0.0.1-build20210629
+    docker.io/rancher/klipper-helm:v0.6.1-build20210616
 )
 
-for image in ${ironbank_images[@]}  ; do
-    docker pull registry1.dso.mil/ironbank/rancher-federal/rke2/$image
-    docker tag registry1.dso.mil/ironbank/rancher-federal/rke2/$image ${address}/ironbank/rancher-federal/rke2/$image
-    docker push ${address}/ironbank/rancher-federal/rke2/$image
+for image in ${registry_images[@]}  ; do
+    docker pull $image
+    address_image_name=$(echo $image | cut -d '/' -f 2-)
+    docker tag $image ${address}/${address_image_name}
+    docker push ${address}/${address_image_name}
 done
 
-non_ironbank_images=(
-    rancher/rke2-cloud-provider:v0.0.1-build20210629
+
+tarball_images=(
+
 )
 
-for image in ${non_ironbank_images[@]} ; do
+for image in ${tarball_images[@]} ; do
     docker pull $image
-    docker tag $image ${address}/$image
-    docker push ${address}/$image
+    tarball_name=$(echo ${image} | sed 's/\//-/g' | sed 's/\:/-/g').tar
+    docker save -o tarball_install/${tarball_name}
 done
